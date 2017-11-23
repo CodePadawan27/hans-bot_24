c243ee824684a685193cc22af3dc02fae33c9f7b
Jarno Rostedt <jarno.a.rostedt@student.samk.fi>
2017-11-23 14:41:28 +0200

    Initial hans-bot24 release.

---
+++ hans-bot_24/DiscordBot/Modulit/Komennot.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hansbot.Modulit
+{
+    public class Komennot : ModuleBase<SocketCommandContext>
+    {
+
+        //Toistaa mitä pyydetään
+        [Command("hans")]
+        public async Task Vastaus([Remainder] string repeat)
+        {
+            await Context.Channel.SendMessageAsync(repeat);
+        }
+
+        //Valitsee hanskaskun listasta ja sanoo sen
+        [Command("hanskasku")]
+        public async Task HansKasku()
+        {
+            Random rand = new Random();
+            
+            List<string> kaskut = new List<string> { "Hipsut", "Oispa kaljaa", "Olinpa höpsö","SQL is the way to go", "Heh heh koodaaminen on hauskaa", "SQL-kielessä on primääriavain ja viiteavain", "Onpa meillä täällä hauskaa" };
+
+            int valittukaskuIndeksi = rand.Next(kaskut.Count);
+
+            await Context.Channel.SendMessageAsync((string)kaskut[valittukaskuIndeksi]);
+        }
+
+        //TODO 
+        //hans-botin helppi
+        [Command("hanshelp")]
+        public async Task HansHelp()
+        {
+
+            string helppi = "Hei olen hans-bot ja olen täällä juuri sinua varten.\n" +
+                            "\nTässä komentoni:" +
+                            "\n!hanshelp - kirjoitit juuri tämän, lol" +
+                            "\n!hans - toistan kaiken mitä sanot" +
+                            "\n!hanskasku - kerron sinulle yhden viisauksistani";
+
+            await Context.Channel.SendMessageAsync(helppi);  
+        }
+
+    }
+}
---
+++ hans-bot_24/DiscordBot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace hansbot
+{
+    public class Program
+    {
+        static void Main(string[] args) => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+            await _client.LoginAsync(TokenType.Bot, "Mzc3NzQ0NTEzMzU3MDUzOTUy.DOTNag.cunIt9A0uHwOh7PT12QTgUJKTfQ");
+            await _client.StartAsync();
+            _handler = new CommandHandler(_client);
+            await Task.Delay(-1);
+        }   
+    }
+}
